45ee8a2471b7b217b4e66ac48d158ce3b837ecab
rvishakhs <rvishakhs@gmail.com>
2023-01-07 23:59:31 +0000

    working on header

--- src/scenes/global/Navbar.jsx
+++ src/scenes/global/Navbar.jsx
@@ -1,8 +1,11 @@
 import { Badge, Box } from '@mui/material'
 import React from 'react'
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
-
+import { HiOutlineShoppingBag } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
 import { CgProfile } from "react-icons/cg";
+import { RxHamburgerMenu } from "react-icons/rx";
+import {icon} from "../../assets/logo_headef"
 
 function Navbar() {
   return (
@@ -16,11 +19,23 @@ function Navbar() {
             />
             <h2 className='text-2xl '>Zalya</h2>
         </div>
-        <Box className='flex gap-2 items-center'>
-        <Badge badgeContent={4} color="primary">
-            <AccountCircleOutlinedIcon />
-        </Badge>
+        <Box className='flex gap-6 items-center cursor-pointer'>
+            
+                <BiSearch 
+                    className="h-6 w-6"
+                />         
+            <Badge badgeContent={4} color="primary">
+                <CgProfile 
+                    className="h-6 w-6"
+                />
+            </Badge>
+            <Badge badgeContent={0} color="primary">
+                <HiOutlineShoppingBag 
+                    className="h-6 w-6"
+                />
+            </Badge>
 
+            <RxHamburgerMenu className="h-6 w-6"/>
         </Box>
     </header>
   )
