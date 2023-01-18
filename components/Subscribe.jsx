3374b66408fd2dc634f240f877e1520e0b56f991
rvishakhs <rvishakhs@gmail.com>
2023-01-18 00:11:38 +0000

    working on newsletter field

--- components/Subscribe.jsx
+++ components/Subscribe.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { SlEnvolopeLetter } from "react-icons/sl";
 
 function Subscribe() {
   return (
-    <div>SubScribe</div>
+    <div className='flex flex-col my-2 mt-4 justify-center w-full max-w-6xl mx-auto'>
+         <div className='flex justify-center w-full mx-auto'>
+            <SlEnvolopeLetter className='w-7 h-7'/>
+         </div>
+         <div className='flex justify-center my-2 w-full mx-auto'>
+            <h2 className='font-serif text-lg uppercase'>SubScribe To Our NewsLetter</h2> 
+         </div>
+         <div className='flex justify-center w-full mx-auto'>
+            <h3 className='font-serif text-base'>and receive $20 coupon for your first order when you checkout</h3> 
+         </div>
+        <div className=''>
+        </div>
+    </div>
   )
 }
 
