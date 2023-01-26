1cb91b381f92445ec98cacaf5c2798e08b4aa0b2
rvishakhs <rvishakhs@gmail.com>
2023-01-26 00:10:48 +0000

    Done just stripe checkout

--- src/scenes/checkout/Payment.jsx
+++ src/scenes/checkout/Payment.jsx
@@ -1,34 +1,48 @@
+import { Box, TextField } from '@mui/material'
 import React from 'react'
 
-function Payment() {
+function Payment({
+    values,
+    type,
+    errors,
+    touched,
+    handleBlur,
+    handleChange,
+    setFieldValue,
+}) {
   return (
-    <Box
-        className='w-full md:w-[80%] grid gap-3 mt-4 md:grid-cols-4 grid-cols-2'
-    >
-        <TextField 
-            className='grid col-span-2'
-            fullWidth
-            type="text"
-            label="Email Id"
-            onBlur={handleBlur}
-            onChange={handleChange}
-            value={values.}
-            name={formattedName("firstName")}
-            error={formattedError("firstName")}
-            helperText={formattedHelper("firstName")}
-        />
-        <TextField 
-            className='grid col-span-2'
-            fullWidth
-            type="text"
-            label="Last Name"
-            onBlur={handleBlur}
-            onChange={handleChange}
-            value={values.lastname}
-            name={formattedName("LastName")}
-            error={formattedError("LastName")}
-            helperText={formattedHelper("LastName")}
-        />
+
+    <Box className='m-7 mx-auto'>
+        <h2 className=' font-bold text-2xl'>Contact Information</h2>
+        <Box
+            className='w-full md:w-[50%] grid gap-3 my-4 md:grid-cols-4 grid-cols-2'
+        >
+            
+            <TextField 
+                className='grid col-span-4'
+                fullWidth
+                type="text"
+                label="Email Id"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.email}
+                name={"Email"}
+                error={!!touched.email && !!errors.email} 
+                helperText={touched.email && errors.email}
+            />
+            <TextField 
+                className='grid col-span-4'
+                fullWidth
+                type="text"
+                label="Phone Number"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.Phonenumber}
+                name={"Phonenumber"}
+                error={!!touched.Phonenumber && !!errors.Phonenumber}
+                helperText={touched.Phonenumber && errors.Phonenumber}
+            />
+        </Box>
     </Box>
   )
 }
