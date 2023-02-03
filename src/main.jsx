15980fe3310f1eb03639778c6f7839677f955f71
rvishakhs <rvishakhs@gmail.com>
2023-02-03 23:37:31 +0000

    this is the worst build since ever

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { CssBaseline } from '@mui/material'
 import { Provider } from 'react-redux'  // need to import for redux initialization
 import { store } from '../redux/config' // imported from redux config file  & wrap the entire components with Provider and pass the store
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-        {/* <CssBaseline /> */}
         <App />
     </Provider>
   </React.StrictMode>,
